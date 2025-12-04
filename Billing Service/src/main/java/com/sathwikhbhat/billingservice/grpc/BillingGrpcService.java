608ca6237760ae1e3251b6faed4268a52b7c671d
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-12-04 21:30:46 +0530

    feat: implement BillingGrpcService with mock-based gRPC request logic

---
+++ Billing Service/src/main/java/com/sathwikhbhat/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,27 @@
+package com.sathwikhbhat.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+        log.info("Received request to create billing account {}", billingRequest);
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
