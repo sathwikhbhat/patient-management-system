04c41f1453f633ca47c9140a771729ea6f2c9723
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-12-15 18:27:29 +0530

    feat: add LoginRequestDTO and LoginResponseDTO for authentication handling

---
+++ Auth Service/src/main/java/com/sathwikhbhat/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.authservice.dto;
+
+public class LoginResponseDTO {
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
